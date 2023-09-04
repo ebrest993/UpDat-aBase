f634899346a62c30b6558f7e41f5b3f65400bb88
ebrest993 <elliott.brest@gmail.com>
2023-09-04 08:36:12 -0500

    tracker_db created

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,21 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE deparment (
-  id INT NOT NULL,
-  course_title VARCHAR(30) NOT NULL,
-  course_description TEXT NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  id            INT         NOT NULL,
+  name          VARCHAR(30) NOT NULL  
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  id            INT         NOT NULL,
+  title         VARCHAR(30) NOT NULL,
+  salary        DECIMAL     NOT NULL,
+  department_id INT         NOT NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE classrooms (
-  id INT NOT NULL,
-  building_name VARCHAR(30) NOT NULL,
-  room_number INT NOT NULL,
-  available BOOLEAN NOT NULL,
-  date_updated DATETIME NOT NULL
+  id            INT         NOT NULL,
+  first_name    VARCHAR(30) NOT NULL,
+  last_name     VARCHAR(30) NOT NULL,
+  role_id       INT         NOT NULL,
+  manager_id    INT         NOT NULL
 );
